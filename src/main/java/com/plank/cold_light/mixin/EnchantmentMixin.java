827409975d32f75f69b89e3d9c876685ec2df8d4
RedStarOverPlank <2900517834@qq.com>
2025-06-09 23:51:35 +0800

    unknow

--- src/main/java/com/plank/cold_light/mixin/EnchantmentMixin.java
+++ src/main/java/com/plank/cold_light/mixin/EnchantmentMixin.java
@@ -1,23 +1,23 @@
 package com.plank.cold_light.mixin;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
+
 import org.spongepowered.asm.mixin.Mixin;
-import net.minecraft.world.item.enchantment.EnchantmentCategory;
-import net.dries007.tfc.common.items.JavelinItem;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.dries007.tfc.common.items.JavelinItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+
 @Mixin(Enchantment.class)
 public abstract class EnchantmentMixin {
-
     @Inject(method = "canEnchant", at = @At("HEAD"), cancellable = true)
-    private void allowJavelinEnchantments(ItemStack p_44689_, CallbackInfoReturnable<Boolean> cir) {
+    private void Javelin(ItemStack p_44689_, CallbackInfoReturnable<Boolean> cir) {
         Item item = p_44689_.getItem();
         if (item instanceof JavelinItem) {
-            Enchantment self = (Enchantment)(Object)this;
-
+            Enchantment self = (Enchantment) (Object) this;
             // 允许三叉戟附魔应用于标枪
             if (self.category == EnchantmentCategory.TRIDENT) {
                 cir.setReturnValue(true);
